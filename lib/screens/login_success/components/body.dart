73780e6821a8ef2d80cf369ae7aeba71205d07a2
ElXak <gelxak@gmail.com>
2021-01-28 14:50:38 +0500

    Refactored SizedBoxes to Paddings

--- lib/screens/login_success/components/body.dart
+++ lib/screens/login_success/components/body.dart
@@ -8,12 +8,16 @@ class Body extends StatelessWidget {
   Widget build(BuildContext context) {
     return Column(
       children: [
-        SizedBox(height: SizeConfig.screenHeight * 0.04),
-        Image.asset(
-          'assets/images/success.png',
-          height: SizeConfig.screenHeight * 0.4, // 40%
+        Padding(
+          padding: EdgeInsets.only(
+            top: SizeConfig.screenHeight * 0.04,
+            bottom: SizeConfig.screenHeight * 0.08,
+          ),
+          child: Image.asset(
+            'assets/images/success.png',
+            height: SizeConfig.screenHeight * 0.4, // 40%
+          ),
         ),
-        SizedBox(height: SizeConfig.screenHeight * 0.08),
         Text(
           'Login Success',
           style: TextStyle(
