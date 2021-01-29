a4f390497731cfeb9449473b137cdb21454dd4c3
ElXak <gelxak@gmail.com>
2021-01-29 11:28:14 +0500

    Discount Banner

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
+      title: 'Lamode.TJ',
       theme: theme(),
       // home: SplashScreen(),
       initialRoute: SplashScreen.routeName,
--- lib/screens/home/components/body.dart
+++ lib/screens/home/components/body.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 import '../../../size_config.dart';
 import '../../../components/home_header.dart';
+import 'discount_banner.dart';
 
 class Body extends StatelessWidget {
   @override
@@ -12,6 +13,8 @@ class Body extends StatelessWidget {
           children: [
             SizedBox(height: getProportionateScreenWidth(20)),
             HomeHeader(),
+            SizedBox(height: getProportionateScreenWidth(20)),
+            DiscountBanner(),
           ],
         ),
       ),
