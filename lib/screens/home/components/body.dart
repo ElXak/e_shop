0fc94c6697f630027af3f6d4b2af9defb9620a2f
ElXak <gelxak@gmail.com>
2021-01-31 00:13:21 +0500

    Categories in Home

--- lib/screens/home/components/body.dart
+++ lib/screens/home/components/body.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 import '../../../size_config.dart';
 import '../../../components/home_header.dart';
+import 'categories.dart';
 import 'discount_banner.dart';
 
 class Body extends StatelessWidget {
@@ -15,6 +16,8 @@ class Body extends StatelessWidget {
             HomeHeader(),
             SizedBox(height: getProportionateScreenWidth(20)),
             DiscountBanner(),
+            SizedBox(height: getProportionateScreenWidth(20)),
+            Categories(),
           ],
         ),
       ),
---
+++ lib/screens/home/components/categories.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+import '../../../size_config.dart';
+import 'categories_data.dart';
+
+class Categories extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding:
+          EdgeInsets.symmetric(horizontal: getProportionateScreenWidth(20)),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          ...List.generate(
+            categories.length,
+            (index) => CategoryCard(
+              icon: categories[index]['icon'],
+              text: categories[index]['text'],
+              onPress: () {},
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class CategoryCard extends StatelessWidget {
+  const CategoryCard({
+    Key key,
+    @required this.icon,
+    @required this.text,
+    @required this.onPress,
+  }) : super(key: key);
+
+  final String icon, text;
+  final GestureTapCallback onPress;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onPress,
+      child: SizedBox(
+        width: getProportionateScreenWidth(55),
+        child: Column(
+          children: [
+            AspectRatio(
+              aspectRatio: 1,
+              child: Container(
+                padding: EdgeInsets.all(getProportionateScreenWidth(15)),
+                decoration: BoxDecoration(
+                  color: Color(0xFFFFECDF),
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: SvgPicture.asset(icon),
+              ),
+            ),
+            const SizedBox(height: 5),
+            Text(text, textAlign: TextAlign.center),
+          ],
+        ),
+      ),
+    );
+  }
+}
