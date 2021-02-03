f395223fe291ccba1fd6e40a7f6f5b9cd3d066c7
ElXak <gelxak@gmail.com>
2021-02-04 01:33:11 +0500

    added GestureDetector for Favorite in Detail

--- lib/screens/details/components/product_description.dart
+++ lib/screens/details/components/product_description.dart
@@ -31,19 +31,26 @@ class ProductDescription extends StatelessWidget {
         SizedBox(height: 5),
         Align(
           alignment: Alignment.centerRight,
-          child: Container(
-            padding: EdgeInsets.all(getProportionateScreenWidth(15)),
-            width: getProportionateScreenWidth(64),
-            decoration: BoxDecoration(
-              color: product.isFavorite ? Color(0xFFFFE6E6) : Color(0xFFF5F6F9),
-              borderRadius: BorderRadius.only(
-                topLeft: Radius.circular(20),
-                bottomLeft: Radius.circular(20),
+          child: GestureDetector(
+            onTap: () {
+              //TODO inverse the isFavorite property of product
+            },
+            child: Container(
+              padding: EdgeInsets.all(getProportionateScreenWidth(15)),
+              width: getProportionateScreenWidth(64),
+              decoration: BoxDecoration(
+                color:
+                    product.isFavorite ? Color(0xFFFFE6E6) : Color(0xFFF5F6F9),
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(20),
+                  bottomLeft: Radius.circular(20),
+                ),
               ),
-            ),
-            child: SvgPicture.asset(
-              'assets/icons/Heart Icon_2.svg',
-              color: product.isFavorite ? Color(0xFFFF4848) : Color(0xFFDBDEE4),
+              child: SvgPicture.asset(
+                'assets/icons/Heart Icon_2.svg',
+                color:
+                    product.isFavorite ? Color(0xFFFF4848) : Color(0xFFDBDEE4),
+              ),
             ),
           ),
         ),
